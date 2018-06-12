b2048bd244027255ed6e6f16e323c4cc487dd8b0
mhd_dll <dali.mhmd@gmail.com>
2018-06-13 02:20:55 +0300

    fix the new solution

--- pluginservice/src/main/java/app/test/pluginservice/StartServiceActivity.java
+++ pluginservice/src/main/java/app/test/pluginservice/StartServiceActivity.java
@@ -3,11 +3,14 @@ package app.test.pluginservice;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
 
 public class StartServiceActivity extends Activity {
     public static String TAG = "StartServiceActivity";
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,12 @@ public class StartServiceActivity extends Activity {
         Log.v(TAG, "onCreate: Starting the service now");
         Intent intent = new Intent(this, SpeechRecognitionService.class);
         startService(intent);
-        finish();
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                StartServiceActivity.this.finish();
+            }
+        }, 2000);
     }
 }
